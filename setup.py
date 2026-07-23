6cad09bb545153c2f5fcebfea3c5eb10ffb4dd05
Skladanoff <skladanov2@yandex.ru>
2026-07-23 23:05:27 +0300

    update

--- setup.py
+++ setup.py
@@ -3,23 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='helloworld-plugin',
     version='0.1.0',
-    packages=find_packages(where='src'),
-    package_dir={"": "src"},
-    include_package_data=True,
-    entry_points={
-        'testy': [
-            'helloworld-plugin=helloworld_plugin:HelloWorldConfig'
-        ]
-    },
-)
-
-from setuptools import find_packages, setup
-
-setup(
-    name='helloworld-plugin',
-    version='0.2.0',
-    description='Example of testy plugin',
-    install_requires=['openpyxl==3.1.1'],
+    description='My first plugin',
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
